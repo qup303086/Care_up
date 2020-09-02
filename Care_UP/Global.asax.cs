d9be2825cc5c90da18f27841e51efca536b02c34
pingping <qup303086@gmail.com>
2020-09-02 14:32:57 +0800

    新增跨域

--- Care_UP/Global.asax.cs
+++ Care_UP/Global.asax.cs
@@ -19,5 +19,12 @@ namespace Care_UP
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest()
+        {
+            if (Request.Headers.AllKeys.Contains("Origin") && Request.HttpMethod == "OPTIONS")
+            {
+                Response.End();
+            }
+        }
     }
 }
