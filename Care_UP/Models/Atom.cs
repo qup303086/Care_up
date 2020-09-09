ab55430bfd4ed025609cfb72849276fe8a7d1d4d
pingping <qup303086@gmail.com>
2020-09-09 11:17:19 +0800

    test

---
+++ Care_UP/Models/Atom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace Care_UP.Models
+{
+    /// <summary>執行結果基元</summary>
+    public class Atom <T> : HttpResponseMessage
+    {
+        /// <summary>是否成功</summary>
+        public bool IsSuccess { get; set; }
+
+        /// <summary>訊息</summary>
+        public string Message { get; set; }
+    }
+}
