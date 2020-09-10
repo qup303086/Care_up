1731e9bd9e2b413e55bda6c63636876f88e7f4b4
pingping <qup303086@gmail.com>
2020-09-10 13:20:13 +0800

    金流

--- Care_UP/Models/Pay.cs
+++ Care_UP/Models/Pay.cs
@@ -7,9 +7,9 @@ using System.Web;
 
 namespace Care_UP.Models
 {
-    public class DetailedDates
+    public class Pay
     {
-        [Key]
+        [Key]//強制主鍵
         [Display(Name = "編號")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
@@ -20,12 +20,11 @@ namespace Care_UP.Models
         public virtual Orders Orders { set; get; }
 
 
-        [Display(Name = "日期")]
-        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public DateTime? NowDateTime { set; get; }
+        [Display(Name = "訊息")]
+        public string Status { get; set; }
 
-         
-    
+        [Display(Name = "付款訊息")]
+        public string Message { get; set; }
 
     }
 }
