f03a1dae84ef8ce934ee648a9dce6db7807ab9fa
lr6 <skysix5678@gmail.com>
2020-09-24 11:36:34 +0800

    家屬中斷 照服員中斷 列舉修改03 中斷>家屬中斷 列舉增加07 照服員中斷

--- Care_UP/Controllers/OrdersController.cs
+++ Care_UP/Controllers/OrdersController.cs
@@ -36,20 +36,6 @@ namespace Care_UP.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, new { result = "訂單取消成功" });
         }
 
-        [Route("InterruptOrder")]
-        [HttpPatch]
-        public IHttpActionResult InterruptOrder(int Id)
-        {
-            Orders orders = db.Orders.Find(Id);
-
-            orders.Status = OrderType.中斷;
-            orders.EditDate = DateTime.Now;
-            db.SaveChanges();
-            return Ok(new
-            {
-                message = "進行中訂單已中斷"
-            });
-        }
 
         // POST: api/Orders
         [ResponseType(typeof(Orders))]
@@ -108,7 +94,9 @@ namespace Care_UP.Controllers
 
 
             var order5 = orderID.Where(x => x.Star != null)
-                 .Where(x => x.Status == OrderType.已取消 || x.Status == OrderType.已完成 || x.Status == OrderType.中斷 || x.Status == OrderType.待退款 || x.Status == OrderType.照服員拒接)
+                 .Where(x => x.Status == OrderType.已取消 || x.Status == OrderType.已完成
+                                                       || x.Status == OrderType.家屬中斷 || x.Status == OrderType.照服員中斷
+                                                       || x.Status == OrderType.待退款 || x.Status == OrderType.照服員拒接)
                  .ToList();
             status[4] = order5.Count;
 
@@ -124,9 +112,9 @@ namespace Care_UP.Controllers
         public IHttpActionResult AttendantsOrderStatus(int id) //照服員id
         {
             int[] status = new int[5];
-            List<Orders> orderID= db.Orders.Where(x => x.AttendantId == id).ToList();
+            List<Orders> orderID = db.Orders.Where(x => x.AttendantId == id).ToList();
 
-            var order1 = orderID.Where(x =>x.Status == OrderType.等待照服員確認訂單).ToList();
+            var order1 = orderID.Where(x => x.Status == OrderType.等待照服員確認訂單).ToList();
 
             status[0] = order1.Count;
 
@@ -144,7 +132,9 @@ namespace Care_UP.Controllers
 
 
             var orders5 = orderID
-                .Where(x => x.Status == OrderType.已取消 || x.Status == OrderType.已完成 || x.Status == OrderType.中斷 || x.Status == OrderType.待退款 || x.Status == OrderType.照服員拒接)
+                .Where(x => x.Status == OrderType.已取消 || x.Status == OrderType.已完成
+                                                      || x.Status == OrderType.家屬中斷 || x.Status == OrderType.照服員中斷
+                                                      || x.Status == OrderType.待退款 || x.Status == OrderType.照服員拒接)
                 .ToList();
             status[4] = orders5.Count;
 
@@ -169,17 +159,18 @@ namespace Care_UP.Controllers
                     message = "目前尚無未確認訂單"
                 });
             }
+
             foreach (Orders item in order)
             {
-                //訂單的開始日期前3天還不接==拒接
-                if (DateTime.Compare(DateTime.Now, item.StartDate.AddDays(-3)) > 0)
+                //訂單的開始日期那天還不接=拒接
+                if (DateTime.Compare(DateTime.Now, item.StartDate) > 0)
                 {
                     item.Status = OrderType.照服員拒接;
                 }
             }
 
             db.SaveChanges();
-            var orders = order.Select(x => new
+            var orders = order.OrderByDescending(x => x.StartDate).Select(x => new
             {
                 x,
                 startDate = x.StartDate.ToString("yyyy-MM-dd"),
@@ -206,26 +197,27 @@ namespace Care_UP.Controllers
                     message = "目前尚無未確認訂單"
                 });
             }
-            foreach (Orders item in order)
-            {
-                if (DateTime.Compare(DateTime.Now, item.StartDate.AddDays(-3)) > 0)
-                {
-                    item.Status = OrderType.照服員拒接;
-                }
-            }
 
-            var orders = order.Select(x => new
+            //foreach (Orders item in order)
+            //{
+            //    if (DateTime.Compare(DateTime.Now, item.StartDate.AddDays(-3)) > 0)
+            //    {
+            //        item.Status = OrderType.照服員拒接;
+            //    }
+            //}
+
+            var orders = order.OrderByDescending(x => x.StartDate).Select(x => new
             {
                 x,
                 startDate = x.StartDate.ToString("yyyy-MM-dd"),
                 endDate = x.EndDate.ToString("yyyy-MM-dd"),
                 OrderInitDate = x.InitDate?.ToString("yyyy-MM-dd"),
-                
+
             });
             return Ok(new
             {
                 orders,
-                count =  orders.Count()
+                count = orders.Count()
             });
 
         }
@@ -244,6 +236,7 @@ namespace Care_UP.Controllers
                     message = "尚無待處理訂單"
                 });
             }
+
             List<Orders> order11 = ordes.Where(x => x.Status == OrderType.待付款).ToList();
             foreach (Orders item in order11)
             {
@@ -252,6 +245,7 @@ namespace Care_UP.Controllers
                     item.Status = OrderType.未於訂單開始前付款;
                 }
             }
+
             List<Orders> order12 = ordes.Where(x => x.Status == OrderType.已付款).ToList();
             foreach (Orders item in order12)
             {
@@ -260,6 +254,7 @@ namespace Care_UP.Controllers
                     item.Status = OrderType.服務進行中;
                 }
             }
+
             db.SaveChanges();
 
             var order = db.Orders.Where(x => x.Elders.MemberId == id).Where(x => x.Status == OrderType.待付款 || x.Status == OrderType.已付款).ToList();
@@ -279,12 +274,12 @@ namespace Care_UP.Controllers
                     startDate = x.StartDate.ToString("yyyy-MM-dd"),
                     endDate = x.EndDate.ToString("yyyy-MM-dd"),
                     OrderInitDate = x.InitDate?.ToString("yyyy-MM-dd"),
-                    OrderStatus =x.Status.ToString()
+                    OrderStatus = x.Status.ToString()
                 });
                 return Ok(new
                 {
                     orders,
-                   count= orders.Count()
+                    count = orders.Count()
                 });
             }
         }
@@ -329,7 +324,7 @@ namespace Care_UP.Controllers
             }
             else
             {
-                var orders = order.Select(x => new
+                var orders = order.OrderByDescending(x => x.StartDate).Select(x => new
                 {
                     x,
                     startDate = x.StartDate.ToString("yyyy-MM-dd"),
@@ -387,19 +382,19 @@ namespace Care_UP.Controllers
             }
             else
             {
-                var orders = order.Select(x => new
+                var orders = order.OrderByDescending(x => x.StartDate).Select(x => new
                 {
                     x,
                     startDate = x.StartDate.ToString("yyyy-MM-dd"),
                     endDate = x.EndDate.ToString("yyyy-MM-dd"),
                     OrderInitDate = x.InitDate?.ToString("yyyy-MM-dd"),
                     OrderStatus = Enum.Parse(typeof(OrderType), x.Status.ToString()).ToString(),
-                    
+
                 });
                 return Ok(new
                 {
                     orders,
-                    count =  orders.Count()
+                    count = orders.Count()
                 });
             }
         }
@@ -445,14 +440,14 @@ namespace Care_UP.Controllers
             }
             else
             {
-                var orders = order.Select(x => new
+                var orders = order.OrderByDescending(x => x.StartDate).Select(x => new
                 {
                     x,
                     startDate = x.StartDate.ToString("yyyy-MM-dd"),
                     endDate = x.EndDate.ToString("yyyy-MM-dd"),
                     OrderInitDate = x.InitDate?.ToString("yyyy-MM-dd"),
                     OrderStatus = Enum.Parse(typeof(OrderType), x.Status.ToString()).ToString(),
-             
+
                 });
                 return Ok(new
                 {
@@ -468,7 +463,9 @@ namespace Care_UP.Controllers
         public IHttpActionResult MemberGet13(int id)
         {
             List<Orders> orders = db.Orders.Where(x => x.Elders.MemberId == id && x.Star == null)
-                .Where(x => x.Status == OrderType.已完成 || x.Status == OrderType.照服員待收款).ToList();
+                .Where(x => x.Status == OrderType.已完成 || x.Status == OrderType.照服員待收款
+                            || x.Status ==OrderType.家屬中斷 || x.Status == OrderType.照服員中斷
+                            ).ToList();
             if (orders.Count == 0)
             {
                 return Ok(new
@@ -476,13 +473,13 @@ namespace Care_UP.Controllers
                     message = "尚無需要填寫評價的訂單"
                 });
             }
-            var order = orders.Select(x => new
+            var order = orders.OrderByDescending(x => x.StartDate).Select(x => new
             {
                 x,
                 initTime = x.InitDate.Value.ToString("yyyy-MM-dd"),
                 startTime = x.StartDate.ToString("yyyy-MM-dd"),
                 endTime = x.EndDate.ToString("yyyy-MM-dd"),
-                status ="待評價"
+                status = "待評價"
             });
             return Ok(new
             {
@@ -503,7 +500,7 @@ namespace Care_UP.Controllers
                 });
             }
 
-            var order = orders.Select(x => new
+            var order = orders.OrderByDescending(x => x.StartDate).Select(x => new
             {
                 x,
                 initTime = x.InitDate.Value.ToString("yyyy-MM-dd"),
@@ -524,7 +521,9 @@ namespace Care_UP.Controllers
         public IHttpActionResult MemberFinish(int id)
         {
             List<Orders> orders = db.Orders.Where(x => x.Elders.MemberId == id)
-                .Where(x => x.Status == OrderType.已取消 || x.Status == OrderType.已完成 || x.Status == OrderType.中斷 || x.Status == OrderType.待退款 || x.Status == OrderType.照服員拒接)
+                .Where(x => x.Status == OrderType.已取消 || x.Status == OrderType.已完成
+                                                      || x.Status == OrderType.家屬中斷 || x.Status == OrderType.照服員中斷
+                                                      || x.Status == OrderType.待退款 || x.Status == OrderType.照服員拒接)
                 .ToList();
             if (orders.Count == 0)
             {
@@ -534,7 +533,7 @@ namespace Care_UP.Controllers
                 });
             }
 
-            var order = orders.Select(x => new
+            var order = orders.OrderByDescending(x => x.StartDate).Select(x => new
             {
                 x,
                 initTime = x.InitDate.Value.ToString("yyyy-MM-dd"),
@@ -542,7 +541,7 @@ namespace Care_UP.Controllers
                 endTime = x.EndDate.ToString("yyyy-MM-dd"),
                 status = Enum.Parse(typeof(OrderType), x.Status.ToString()).ToString(),
                 serviceTime = Utility.Servicetime(Enum.Parse(typeof(ServiceTime), x.Attendants.ServiceTime.ToString()).ToString()),
-       
+
             });
 
             return Ok(new
@@ -556,7 +555,9 @@ namespace Care_UP.Controllers
         public IHttpActionResult AttendantsFinish(int id)
         {
             List<Orders> orders = db.Orders.Where(x => x.AttendantId == id)
-                .Where(x => x.Status == OrderType.已完成 || x.Status == OrderType.中斷 || x.Status == OrderType.待退款 || x.Status == OrderType.照服員拒接)
+                .Where(x => x.Status == OrderType.已取消 || x.Status == OrderType.已完成
+                            || x.Status == OrderType.家屬中斷 || x.Status == OrderType.照服員中斷
+                            || x.Status == OrderType.待退款 || x.Status == OrderType.照服員拒接)
                 .ToList();
             if (orders.Count == 0)
             {
@@ -566,7 +567,7 @@ namespace Care_UP.Controllers
                 });
             }
 
-            var order = orders.Select(x => new
+            var order = orders.OrderByDescending(x => x.StartDate).Select(x => new
             {
                 x,
                 initTime = x.InitDate.Value.ToString("yyyy-MM-dd"),
@@ -574,7 +575,7 @@ namespace Care_UP.Controllers
                 endTime = x.EndDate.ToString("yyyy-MM-dd"),
                 status = Enum.Parse(typeof(OrderType), x.Status.ToString()).ToString(),
                 serviceTime = Utility.Servicetime(Enum.Parse(typeof(ServiceTime), x.Attendants.ServiceTime.ToString()).ToString()),
-       
+
 
             });
 
@@ -645,6 +646,45 @@ namespace Care_UP.Controllers
                 return Ok(new { message = "未填寫拒絕理由" });
             }
         }
+
+        [Route("MemberInterruptOrder")]
+        [HttpPatch]
+        public IHttpActionResult InterruptOrderM(OrderReject orderReject)
+        {
+            if (!string.IsNullOrWhiteSpace(orderReject.Cancel))
+            {
+                Orders order = db.Orders.Find(orderReject.Id);
+                order.Cancel = orderReject.Cancel;
+                order.Status = OrderType.家屬中斷;
+                order.EditDate = DateTime.Now;
+                db.SaveChanges();
+                return Ok(new { message = "進行中訂單已由家屬中斷" });
+            }
+            else
+            {
+                return Ok(new { message = "未填寫中斷理由" });
+            }
+        }
+
+        [Route("AttendantInterruptOrder")]
+        [HttpPatch]
+        public IHttpActionResult InterruptOrderA(OrderReject orderReject)
+        {
+            if (!string.IsNullOrWhiteSpace(orderReject.Cancel))
+            {
+                Orders order = db.Orders.Find(orderReject.Id);
+                order.Cancel = orderReject.Cancel;
+                order.Status = OrderType.照服員中斷;
+                order.EditDate = DateTime.Now;
+                db.SaveChanges();
+                return Ok(new { message = "進行中訂單已由照服員中斷" });
+            }
+            else
+            {
+                return Ok(new { message = "未填寫中斷理由" });
+            }
+        }
+
 
         [Route("WriteLog")]
         [HttpPost]
